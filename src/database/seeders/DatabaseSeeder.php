91af4c5c50d328e6db626e08ecea85cca60b4b75
Subham Saha <subham@Subhams-MacBook-Pro.local>
2023-10-05 21:37:23 +0530

    completed

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             OtherPermissionSeeder::class,
             LegalSeeder::class,
+            SeoSeeder::class,
         ]);
     }
 }
