4e4fe18b17918ea1bea35a98b4f3d938b2c3fae8
Subham Saha <subham@Subhams-MacBook-Pro.local>
2023-10-29 15:20:28 +0530

    global search

--- src/app/Modules/GlobalSearch/Services/GlobalSearchService.php
+++ src/app/Modules/GlobalSearch/Services/GlobalSearchService.php
@@ -16,13 +16,13 @@ class GlobalSearchService
     public function paginateMain(Int $total = 10): LengthAwarePaginator
     {
         $search = request()->query('filter')['search'] ?? '';
-        $query1 = Category::query()->select('id', 'name', 'slug', 'image')->where('is_draft', true)->where('name', 'LIKE', '%'.$search.'%');
-        $query2 = SubCategory::query()->select('id', 'name', 'slug', 'image')->where('is_draft', true)->where('name', 'LIKE', '%'.$search.'%');
-        $query3 = Product::query()->select('id', 'name', 'slug', 'image')->where('is_draft', true)->where('name', 'LIKE', '%'.$search.'%');
+        $query1 = Category::query()->select('id', 'name', 'slug', 'image', 'search_type')->where('is_draft', true)->where('name', 'LIKE', '%'.$search.'%');
+        $query2 = SubCategory::query()->select('id', 'name', 'slug', 'image', 'search_type')->where('is_draft', true)->where('name', 'LIKE', '%'.$search.'%');
+        $query3 = Product::query()->select('id', 'name', 'slug', 'image', 'search_type')->where('is_draft', true)->where('name', 'LIKE', '%'.$search.'%');
         $query4 = $query3->union($query2);
         $query = $query4->union($query1);
         return QueryBuilder::for($query)
-                ->allowedFields(['id', 'name', 'slug', 'image'])
+                ->allowedFields(['id', 'name', 'slug', 'image', 'search_type'])
                 ->defaultSort('-id')
                 ->allowedSorts('id', 'name')
                 ->allowedFilters([
