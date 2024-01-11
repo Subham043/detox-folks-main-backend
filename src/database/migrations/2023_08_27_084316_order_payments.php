1e50ccb0499c5d1e5fd34bee6e36d5824559d721
Subham Saha <subham@Subhams-MacBook-Pro.local>
2024-01-12 01:52:04 +0530

    done

--- src/database/migrations/2023_08_27_084316_order_payments.php
+++ src/database/migrations/2023_08_27_084316_order_payments.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('mode', 500);
             $table->string('status', 500);
             $table->text('payment_data')->nullable();
-            $table->string('razorpay_signature', 255)->nullable();
+            $table->text('razorpay_signature')->nullable();
             $table->text('razorpay_order_id')->nullable();
             $table->text('razorpay_payment_id')->nullable();
             $table->foreignId('order_id')->nullable()->constrained('orders')->nullOnDelete();
