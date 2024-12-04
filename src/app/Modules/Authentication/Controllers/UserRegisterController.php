72f292f9dc862b9ebe13c9345f2640e2bfb20ade
subham043@ <sahasubham043@gmail.com>
2024-12-04 16:19:13 +0530

    done

--- src/app/Modules/Authentication/Controllers/UserRegisterController.php
+++ src/app/Modules/Authentication/Controllers/UserRegisterController.php
@@ -6,18 +6,15 @@ use App\Http\Controllers\Controller;
 use App\Http\Services\RateLimitService;
 use App\Modules\Authentication\Requests\UserRegisterPostRequest;
 use App\Modules\Authentication\Resources\AuthCollection;
-use App\Modules\Authentication\Services\AuthService;
 use App\Modules\User\Services\UserService;
 use Illuminate\Auth\Events\Registered;
 
 class UserRegisterController extends Controller
 {
-    private AuthService $authService;
     private UserService $userService;
 
-    public function __construct(AuthService $authService, UserService $userService)
+    public function __construct(UserService $userService)
     {
-        $this->authService = $authService;
         $this->userService = $userService;
     }
 
@@ -28,17 +25,14 @@ class UserRegisterController extends Controller
             'email' => !empty($request->email) ? $request->email : null
         ]);
         $this->userService->syncRoles(['User'], $user);
-        $token = $this->authService->generate_token($user);
         if($user->email){
             event(new Registered($user));
         }
 
-        if ($token) {
+        if ($user) {
             (new RateLimitService($request))->clearRateLimit();
             return response()->json([
                 'message' => 'Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.',
-                'token_type' => 'Bearer',
-                'token' => $token,
                 'user' => AuthCollection::make($user),
             ], 201);
         }
